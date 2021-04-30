4f3ee18e7bcfaa4f4e44bad718d8865ced98bbd4
Сергей Грищенков <grishenkov.sergei@gmail.com>
2021-04-30 20:03:13 +0300

    исправил пути

--- src/index.js
+++ src/index.js
@@ -16,11 +16,9 @@ const store = createStore(
 
 Promise.all([store.dispatch(ActionCreator.addInitialPic())]).then(() => {
   ReactDOM.render(
-      <React.StrictMode>
         <Provider store={store}>
           <App />
-        </Provider>
-      </React.StrictMode>,
+        </Provider>,
       document.getElementById(`root`)
   );
 });
