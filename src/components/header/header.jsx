a2a471f4bb4dce50c932f198fa5718ab8a832e48
Сергей Грищенков <grishenkov.sergei@gmail.com>
2022-08-31 01:12:24 +0300

    added Link's to Header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -48,33 +48,33 @@ function Header({ selectedGuitarsID }) {
             <button className="header__nav-item-close" onClick={handleBurgerClose}></button>
           </li>
           <li className="header__nav-item">
-            <a tabIndex={0} className="header__nav-item-link" href="/" onClick={() => handleCatalog}>Каталог</a>
+            <Link tabIndex={0} className="header__nav-item-link" to="/" onClick={() => handleCatalog}>Каталог</Link>
           </li>
           <li className="header__nav-item">
-            <a tabIndex={0} className="header__nav-item-link" href="/"> Где купить?</a>
+            <Link tabIndex={0} className="header__nav-item-link" to="/"> Где купить?</Link>
           </li>
           <li className="header__nav-item">
-            <a tabIndex={0} className="header__nav-item-link" href="/">О компании</a>
+            <Link tabIndex={0} className="header__nav-item-link" to="/">О компании</Link>
           </li>
           <li className="header__nav-item header__nav-item--services">
-            <a tabIndex={0} className="header__nav-item-link" href="/">Cервис-центры</a>
+            <Link tabIndex={0} className="header__nav-item-link" to="/">Cервис-центры</Link>
           </li>
           <li className="header__nav-item header__nav-item--shops">
-            <a tabIndex={0} className="header__nav-item-link" href="/">Магазины</a>
+            <Link tabIndex={0} className="header__nav-item-link" to="/">Магазины</Link>
           </li>
         </ul>
         <ul className="header__user">
           <li className="header__user-item header__user-item--location">
-            <a tabIndex={0} className="header__user-item-link" href="/">Location</a>
+            <Link tabIndex={0} className="header__user-item-link" to="/">Location</Link>
           </li>
           <li className="header__user-item header__user-item--search">
-            <a tabIndex={0} className="header__user-item-link" href="/">Search</a>
+            <Link tabIndex={0} className="header__user-item-link" to="/">Search</Link>
           </li>
           <li className="header__user-item header__user-item--cart">
-            <a tabIndex={0} className="header__user-item-link header__user-item-link--cart" href="/" onClick={() => handleCart}>
+            <Link tabIndex={0} className="header__user-item-link header__user-item-link--cart" to="/" onClick={() => handleCart}>
               Cart
               <span className="header__user-item-link-count">{selectedGuitarsID.length > 0 && selectedGuitarsID.length}</span>
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
